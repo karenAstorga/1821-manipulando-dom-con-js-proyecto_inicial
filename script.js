b52faefb0ea8bf3f8071d909a32038d2cb8173c9
karenAstorga <114182867+karenAstorga@users.noreply.github.com>
2022-11-02 17:51:47 -0300

    Manipulación de DOM. Creando módulos JS

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 //Inmediately invoked function expression IIFE Ponemos todo dentro de una función para limitar su scope
+import checkComplete from "./Components/checkcomplete.js";
+import deleteIcon from "./Components/deleteIcon.js";
 
                     const btn= document.querySelector('[data-form-btn]');
 
@@ -17,11 +19,10 @@
                     titleTask.innerText= value;
                     taskContent.appendChild(titleTask);
 
-                    const content= `
-                    <i class="fas fa-trash-alt trashIcon icon"></i>`;
                     //task.innerHTML= content;
                     task.appendChild(taskContent);
                     list.appendChild(task);
+                    task.appendChild(deleteIcon());
 
                 
                     
@@ -30,24 +31,6 @@
 
                 btn.addEventListener("click", createTask)
 
-                const checkComplete= () =>{
-
-                    const i= document.createElement("i");
-                    i.classList.add("far", "fa-check-square", "icon");
-
-                    i.addEventListener("click", completeTask);
-
-                    return i;
-                }
-
-                const completeTask= (event) =>{
-
-                    const element= event.target;
-
-                    //toggle comprueba si esta clase existe, si existe la quita y si no existe la agrega
-                    element.classList.toggle("fas");
-                    element.classList.toggle("completeIcon");
-                    element.classList.toggle("far");
-                }
-
+                
+                
             
